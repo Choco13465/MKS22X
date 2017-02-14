f212ebecfd208a6e29254bdc362e0fcb2818f3e7
Emily Wang <lunareclipsite@gmail.com>
2017-02-14 00:11:17 -0500

    Why does solve 1 work and then everything fails after that? I changed everything I could think of and there's still no difference in results orz

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,59 +1,87 @@
 public class QueenBoard{
-    private int[][]board;
-    private int solutionCount;
-    
-    public QueenBoard(int size){
-	board = new int[size][size];
-	clear();
-	int solutionCount = -1;
+  private int[][]board;
+  private int solutionCount;
+  
+  public QueenBoard(int size){
+    board = new int[size][size];
+    clear();
+    int solutionCount = -1;
+  }
+  
+  public void clear(){
+    for (int y = 0; y < board.length; y++){
+      for (int x = 0; x < board.length; x++){
+        board[y][x] = -1;
+      }
     }
+  }
+  
+  
+  public boolean solve(){
+    return solveH();
+  }
+  public boolean solveH(){
+    boolean possible = false;
+    for (int a = 0; a < board.length - 1 || possible == false; a++){
+      clear();
+      for (int b = 0; b < board.length - 1 || possible == false; b++){
+        if (board[b][a] == -1){
+          possible = fill(b, a);
+        } else {
+          board[b][a] = 1;
+        }
+      }
+    }
+    return possible;
+  }
+  public boolean fill(int x, int y){
+    int count = board.length;
+    for (int a = y; a < board.length; a++){
+      for (int b = x; b < board.length; b++){
+        if (board[b][a] == -1){
+          addQ(b, a);
+          count--;
+          b+=board.length;
+        }
+      }
+    }
+    return count == 0;
+  }
+  public void addQ(int b, int a){
+    board[b][a] = 0;
+    int x = b;
+    int y = a;
+    while (x > 0){
+      x-=1;
+      y+=1;
+      board[x][y] = 1;
+    }
+    x = b;
+    y = a;
+    while (y < board.length -1){
+      y++;
+      board[x][y] = 1;
+    }
+    x = b;
+    y = a;
+    while (x < board.length -1){
+      x++;
+      y++;
+      board[x][y] = 1;
+    }
+  }
+  
+  public int getSolutionCount(){
+    return solutionCount;
+  }
+  public String toString(){
+    return "";
+  }
 
-    clear(){
-	for (int y = 0; y < board.length; y++){
-	    for (int x = 0; x < board.length; x++){
-		board[y][x] = -1;
-	    }
-	}
-    }
-
-
-    public boolean solve(){
-	return solveH(0);
-    }
-    public boolean solveH(int col){
-	boolean possible = false;
-	int count = board.length;
-	for (int y = 0; y < board.length; y++){
-	    for (int x = 0; x < board.length; x++){
-		if (board[x][y] == -1){
-		    addQueen(x, y);
-		    count - 1;
-		}
-	    }
-	}
-	if (count == 0){
-	    possible = true;
-	}
-	return possible;
-    }
-
-    public int getSolutionCount(){
-	return solutionCount;
-    }
-    public String toString(){
-	return "";
-    }
-
-    public void addQueen(int x, int y){
-	board[x][y] = 0;
-	int a = x;
-	int b = y;
-	while(a > -1 && b > -1){
-	    board[a-1][b] = 1;
-	    board[a][b-1] = 1;
-	    board[a-1][b-1] = 1;
-	    a--;
-	    b--;
-	}
-	while(
+  public static void main(String[] args){
+    QueenBoard s = new QueenBoard(1);
+    QueenBoard t = new QueenBoard(2);
+    System.out.println(s.solve());
+    System.out.println(t.solve());
+  }
 }
