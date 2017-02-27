44449b704fd214a412a4446ce6d0f20a6094c151
Emily Wang <lunareclipsite@gmail.com>
2017-02-27 15:36:39 -0500

    It compiles

--- 03NKnight/KnightBoard.java
+++ 03NKnight/KnightBoard.java
@@ -28,18 +28,87 @@ public class KnightBoard{
     }
     for (int b = row; b < R; b++){
       for (int i = col; i < C; i++){
-        try (if (board[b-2][i-2] == 0){
-          board[b][i] = rank;
-          if (solveH(b-2, i-2, rank++)){
-            return true;
-          }else{
-            board[b][i] = 0;
+        if (b-2 >= 0 && i-1 >= 0){
+          if (board[b-2][i-1] == 0){
+            board[b][i] = rank;
+            if (solveH(b-2, i-1, rank++)){
+              return true;
+            }else{
+              board[b][i] = 0;
+            }
           }
         }
-             catch(IndexOutOfBoundsException()
-              
-              
-        
+        if (b-1 >= 0 && i-2 >= 0){
+          if (board[b-1][i-2] == 0){ 
+            board[b][i] = rank;
+            if (solveH(b-1, i-2, rank++)){
+              return true;
+            }else{
+              board[b][i] = 0;
+            }
+          }
+        }
+        if (b+1 <= R && i-2 >= 0){
+          if (board[b+1][i-2] == 0){ 
+            board[b][i] = rank;
+            if (solveH(b+1, i-2, rank++)){
+              return true;
+            }else{
+              board[b][i] = 0;
+            }
+          }
+        }
+        if (b+2 <= R && i-1 >= 0){
+          if (board[b+2][i-1] == 0){ 
+            board[b][i] = rank;
+            if (solveH(b+2, i-1, rank++)){
+              return true;
+              }else{
+                board[b][i] = 0;
+              }
+          }
+        }
+        if (b+2 <= R && i+1 >= 0){
+          if (board[b+1][i+2] == 0){ 
+            board[b][i] = rank;
+            if (solveH(b+1, i+2, rank++)){
+              return true;
+            }else{
+              board[b][i] = 0;
+            }
+          }
+        }
+        if (b+1 <= R && i+2 >= 0){
+          if (board[b+1][i+2] == 0){ 
+            board[b][i] = rank;
+            if (solveH(b+1, i+2, rank++)){
+              return true;
+            }else{
+              board[b][i] = 0;
+            }
+          }
+        }
+        if (b-1 <= R && i+2 >= 0){
+          if (board[b-1][i+2] == 0){ 
+            board[b][i] = rank;
+            if (solveH(b-1, i+2, rank++)){
+              return true;
+            }else{
+              board[b][i] = 0;
+            }
+          }
+        }
+        if (b-2 <= R && i+1 >= 0){
+          if (board[b-2][i+1] == 0){ 
+            board[b][i] = rank;
+            if (solveH(b-2, i+1, rank++)){
+              return true;
+            }else{
+              board[b][i] = 0;
+            }
+          }
+        }
+      }
     }
     return false;
   }
@@ -49,10 +118,22 @@ public class KnightBoard{
     String s = "";
     for (int x = 0; x < board.length; x++){
       for (int y = 0; y < board.length; y++){
-        s = s + " " + board[x][y];
+        if (board[x][y] < 10){
+          s = s + "  " + board[x][y];
+        }else{
+          s = s + " " + board[x][y];
+        }
       }
       s+="\n";
     }
     return s;
   }
+  
+  public static void main(String[] args){
+    QueenBoard blah = new QueenBoard(1);
+    System.out.println(blah.toString());
+    System.out.println(blah.solve());
+    System.out.println(blah.toString());
+  }
 }
+
