55f0083fcba4217e409f3cc05e49158574707ec5
Emily W <lunareclipsite@gmail.com>
2017-03-05 20:17:48 -0500

    One misplaced parenthesis = pain

--- 01sqrt/Recursion.java
+++ 01sqrt/Recursion.java
@@ -6,16 +6,20 @@ public class Recursion{
   }
   public static double sqrt(double n){
     if (n < 0){
-      throw new IllegalArgumentException("n is less than 0!");
+      throw new IllegalArgumentException();
+    }
+    if (n == 0){
+      return n;
     }
     return wrap(n, n / 2);
   }
   public static double wrap(double n, double a){
-    if (((Math.abs((a * a) - n)) / n) * 100 <= 0.0001){
-      return a;
+    double b = ((n/a)+a) / 2;
+    if(Math.abs(b*b)-n < 0.00000000001){
+      return b;
     }
     else {
-      return wrap(n, (n / a + a) / 2);
+      return wrap(n, b);
     }
   }
 }
