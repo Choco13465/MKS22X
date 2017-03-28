41cf474c996ae5083c0d8af770d4f8dfc62ffff4
Emily Wang <lunareclipsite@gmail.com>
2017-03-28 15:34:11 -0400

    Start

---
+++ 09Linked/MyLinkedList.java
@@ -0,0 +1,28 @@
+public class MyLinkedList{
+    public LNode start;
+    public LNode end;
+    public int size;
+
+    public MyLinkedList(){
+	start = null;
+	end = null;
+	size = 0;
+    }
+
+	
+    public void add(int zel){
+	if (size = 0)
+	next = LNode(zel);
+    }
+
+    private class LNode{
+	private int value;
+	private LNode next;
+	
+	public LNode(int e){
+	    value = e;
+	    next = null;
+	}
+    }
+}
+
