08ebbc4de346773643e3772dafc2052e208c5e1d
Emily Wang <lunareclipsite@gmail.com>
2017-03-30 15:34:04 -0400

    code progress

--- 09Linked/MyLinkedList.java
+++ 09Linked/MyLinkedList.java
@@ -9,10 +9,28 @@ public class MyLinkedList{
 	size = 0;
     }
 
+    public String toString(){
+	String s = "[";
+	LNode a = start;
+	while (size != 0){
+	    s += a.value + ", ";
+	}
+s+="]"
+    temp = temp.next;
+if temp == null break out
+
 	
     public void add(int zel){
-	if (size = 0)
-	next = LNode(zel);
+	addH(zel, start);
+    }
+
+    public void addH(int zel, LNode a){
+	if (a.next = null){
+	   a.next = new LNode(zel);
+	   size++;
+	}else{
+	    addH(zel, a.next);
+	}
     }
 
     private class LNode{
