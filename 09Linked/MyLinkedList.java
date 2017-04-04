fd5ac8c460037fe0c090617358b799789648d396
Emily Wang <lunareclipsite@gmail.com>
2017-04-04 15:33:40 -0400

    progress

--- 09Linked/MyLinkedList.java
+++ 09Linked/MyLinkedList.java
@@ -1,6 +1,17 @@
 public class MyLinkedList{
-    public LNode start;
-    public LNode end;
+    private class LNode{
+	private int value;
+	private LNode next, prev;
+	
+	public LNode(int e){
+	   this.value = e;
+	}
+	public String toString(){
+	    return value + "";
+	}
+    }
+
+    public LNode start, end;
     public int size;
 
     public MyLinkedList(){
@@ -9,38 +20,59 @@ public class MyLinkedList{
 	size = 0;
     }
 
+    public int size(){
+	return size;
+    }
+
     public String toString(){
 	String s = "[";
-	LNode a = start;
-	while (size != 0){
-	    s += a.value + ", ";
+	LNode temp = start;
+	int zel = size();
+	while (zel > 0){
+	    s += temp.toString();
+	    temp = temp.next;
+	    zel--;
 	}
-s+="]"
-    temp = temp.next;
-if temp == null break out
+	return s + "]";
+    }
 
 	
-    public void add(int zel){
+    public void add(int value){
+	LNode zel = new LNode(value);
 	addH(zel, start);
     }
 
-    public void addH(int zel, LNode a){
-	if (a.next = null){
-	   a.next = new LNode(zel);
-	   size++;
+    public void addH(LNode node, LNode a){
+	if (a != null){
+	    if (a.next == null){
+		a.next = node;
+		size++;
+	    }else{
+		addH(node, a.next);
+	    }
 	}else{
-	    addH(zel, a.next);
+	    a = node;
+	    size++;
 	}
+    }
+    /*
+  private LNode getNthNode(int n){}
+  private void addAfter(LNode location, LNode toBeAdded){ }
+  private void remove(LNode target){}
+  public int get(int index){}
+  public int set(int index, int value){}
+  public int indexOf(int value){}
+  public int remove(int index){}
+  public void add(int index,int value){}
+    */
+
+    public static void main(String [] args){
+	MyLinkedList a = new MyLinkedList();
+	System.out.println(a.toString());
+	a.add(1);
+	System.out.println(a.size());
+	System.out.println(a.toString());
     }
 
-    private class LNode{
-	private int value;
-	private LNode next;
-	
-	public LNode(int e){
-	    value = e;
-	    next = null;
-	}
-    }
 }
 
