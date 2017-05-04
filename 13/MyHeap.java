5eb5eb99f072f03b9dc36847ec08e16f46280efe
Emily Wang <lunareclipsite@gmail.com>
2017-05-04 15:33:55 -0400

    Progress

--- 13/MyHeap.java
+++ 13/MyHeap.java
@@ -24,5 +24,36 @@ public class MyHeap{
     public void add(String s){
 	size++;
 	list.add(s);
-	while (s.compareTo(list.get((int)(size/2))))
+	pushUp(size);
+    }
+    
+    private void pushUp(int i){
+	while ((constant * (s.compareTo(list.get((int)(i/2))))) > 1 && i > 1){
+	    swap(i, (int)i/2);
+	    i = (int)i/2;
+	}
+    }
+
+    private void swap(int i, int parent){
+	String temp = list.get(i);
+	list.set(i, list.get(parent));
+	list.set(parent, temp);
+    }
+
+private 
+
+    public static void main(String[] args){
+	boolean b = false;
+	String[] stuff = {"34", "23", "45", "20", "56", "17", "12"};
+	String[] sorted = new String[stuff.length];
+	MyHeap alpha = new MyHeap(b);
+	for (int i = 0; i < stuff.length; i++){
+	    alpha.add(stuff[i]);
+	    System.out.println(alpha.peek(i));
+	}
+    }
+}
+
+		    
+	    
 		    
